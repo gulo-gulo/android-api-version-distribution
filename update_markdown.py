ce0aa0e05b5a8547cf7e2fd5c4ff394d0f96beed
justin <justin@justin>
2023-05-17 17:13:43 -0400

    updated

--- update_markdown.py
+++ update_markdown.py
@@ -24,13 +24,16 @@ def create_distribution(distribution_json: dict) -> Distribution:
     )
 
 
-def get_distributions_json_list() -> list[dict]:
-    return json.loads(requests.get(distribution_url).text)
+def get_distributions_text() -> str:
+    return requests.get(distribution_url).text
 
 
-def get_distribution_list() -> list[Distribution]:
+def get_and_store_distributions() -> list[Distribution]:
+    distributions_text = get_distributions_text()
+    with open("distributions.json", "w") as file:
+        file.write(distributions_text)
+    distributions_json_list = json.loads(distributions_text)
     distribution_list: list[Distribution] = []
-    distributions_json_list = get_distributions_json_list()
     for each in distributions_json_list:
         distribution_list.append(create_distribution(each))
     return distribution_list
@@ -56,7 +59,7 @@ def format_decimal(number: float) -> str:
 
 
 def update_markdown():
-    distribution_list = get_distribution_list()
+    distribution_list = get_and_store_distributions()
     current_time = datetime.utcnow().strftime("%m/%d/%Y %H:%M:%S UTC")
     remaining_distribution = get_remaining_distribution(distribution_list)
     distribution_list.append(remaining_distribution)
@@ -67,8 +70,8 @@ def update_markdown():
         f"#### Updated daily",
         f"- Last Updated: {current_time}",
         "",
-        "API | Version | Distribution | Cumulative | Notes |",
-        "| -- | ------ | ------------ | ---------- | ----- |",
+        "| API | Version | Distribution | Cumulative | Notes |",
+        "| --- | ------- | ------------ | ---------- | ----- |",
     ]
     last_index = len(distribution_list) - 1
     percentage_sum = 0
@@ -79,8 +82,11 @@ def update_markdown():
         notes = "Retrieved from [source](#source)"
         if index == last_index:
             notes = "`100% - sum of all rows above`"
-        row = f"|{distribution.api_level} | {distribution.version} | {distribution_pct}%| {cumulative_pct}% | {notes}|"
-
+        row = f"| {distribution.api_level} " \
+              f"| {distribution.version} " \
+              f"| {distribution_pct}% " \
+              f"| {cumulative_pct}% " \
+              f"| {notes} |"
         markdown_lines.append(row)
     write_lines_to_markdown(markdown_lines)
 
